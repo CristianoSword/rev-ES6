3b484d6d7018b0b870f4dcd067ae697f3372e1d8
cristianoSword <originalvgo@gmail.com>
2022-11-03 15:57:39 -0300

    arrow function

---
+++ rev.js
@@ -0,0 +1,16 @@
+
+//var x = 0
+let x = 0
+
+const CONST_ID = 0 //convention
+
+//arrow function - no this, no proto, no construct ES6
+
+//es5
+function func (a, b, c) {} //func declaration 
+var func = function (a, b, c) {} //func expression 
+//es6
+let func = (a) => {} // parenteses opcional como um parametro
+let func = (a, b ,c) => {} //parenteses requerido com multiplos parametros
+
+
